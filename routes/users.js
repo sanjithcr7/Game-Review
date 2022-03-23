31a9700e3eda09c1d34a5883162eefaecae877f0
sanjithcr7 <92458509+sanjithcr7@users.noreply.github.com>
2022-03-23 18:16:55 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 let mongoose=require('mongoose');
-// mongoose.connect('mongodb+srv://Chandana:cvml462ccl4*@mytasker.wknnc.mongodb.net/gaming-forum?retryWrites=true&w=majority')
-mongoose.connect('mongodb://localhost:27017/gaming-forum');
+mongoose.connect('mongodb+srv://Chandana:cvml462ccl4*@mytasker.wknnc.mongodb.net/gaming-forum?retryWrites=true&w=majority')
+//mongoose.connect('mongodb://localhost:27017/gaming-forum');
 let schema=mongoose.Schema(
   {
     name:{
